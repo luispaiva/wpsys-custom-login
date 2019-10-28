3725b99936b08e40825b28ecc4441f16ee4e9379
Luis Paiva <contato@luispaiva.com.br>
2019-10-28 10:52:43 -0300

    Customização js necessária para o captcha

--- assets/js/custom-login.js
+++ assets/js/custom-login.js
@@ -2,10 +2,12 @@ jQuery( function($) {
     /* ======================= Form Login ======================= */
     $( '#loginform input[type="text"]' ).attr( 'placeholder', WPSysCustomLogin.textUsername );
     $( '#loginform input[type="password"]' ).attr( 'placeholder',  WPSysCustomLogin.textPassword );
+    $( '#loginform input[type="text"].captcha' ).attr( 'placeholder',  WPSysCustomLogin.textCode );
 
     /* ======================= Form Register ======================= */
     $( '#registerform input[type="text"]' ).attr( 'placeholder',  WPSysCustomLogin.textNameUser );
     $( '#registerform input[type="email"]' ).attr( 'placeholder', WPSysCustomLogin.textEmail );
+    $( '#registerform input[type="text"].captcha' ).attr( 'placeholder',  WPSysCustomLogin.textCode );
 
     /* ======================= Form Lost Password ======================= */
     $( '#lostpasswordform input[type="text"]' ).attr( 'placeholder', WPSysCustomLogin.textNameOrEmail );
